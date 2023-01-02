86b40bb60770b101bb502fffda56cdd215b6bdde
rogerio.chaves <rogerio.chaves@kentech.com.br>
2023-01-02 07:49:49 -0300

    Add coleção de produtos e inicialização dela

--- APICatalogo/Models/Categoria.cs
+++ APICatalogo/Models/Categoria.cs
@@ -1,10 +1,20 @@
-﻿namespace APICatalogo.Models
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace APICatalogo.Models
 {
     public class Categoria
     {
+        public Categoria()
+        {
+            Produtos = new Collection<Produto>();
+        }
+
         public int CategoriaId { get; set; }
         public string Nome { get; set; }
         public string ImgURL { get; set; }
 
+        public ICollection<Produto> Produtos { get; set; }
     }
 }
