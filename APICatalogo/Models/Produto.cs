f298df6da7738de6615e5d6b65945366341f7200
rogerio.chaves <rogerio.chaves@kentech.com.br>
2023-01-02 07:36:24 -0300

    Criação de models e suas Propiedades

---
+++ APICatalogo/Models/Produto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace APICatalogo.Models
+{
+    public class Produto
+    {
+        public int ProdutoId { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public decimal Preco { get; set; }
+        public string ImgURL { get; set; }
+        public float Estoque { get; set; }
+        public DateTime DataCadastro { get; set; }
+    }
+}
