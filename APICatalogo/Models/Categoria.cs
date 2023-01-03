fe2e90a345d21e3847db276f2665f724838d67ec
rogerio.chaves <rogerio.chaves@kentech.com.br>
2023-01-03 07:51:44 -0300

    Criação de DataAnotations na model Categoria

--- APICatalogo/Models/Categoria.cs
+++ APICatalogo/Models/Categoria.cs
@@ -1,18 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace APICatalogo.Models
 {
+    [Table("Categorias")]
     public class Categoria
     {
         public Categoria()
         {
             Produtos = new Collection<Produto>();
         }
-
+        [Key]
         public int CategoriaId { get; set; }
+        [Required]
+        [MaxLength(100)]
         public string Nome { get; set; }
+        [Required]
+        [MaxLength(300)]
         public string ImgURL { get; set; }
 
         public ICollection<Produto> Produtos { get; set; }
