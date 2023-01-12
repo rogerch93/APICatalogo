ed19e5c0aca52c26d77cfd5c54745b0aaed8112e
rogerio.chaves <rogerio.chaves@kentech.com.br>
2023-01-12 09:09:24 -0300

    Criação de CategoriaController

---
+++ APICatalogo/Controllers/CategoriasController.cs
@@ -0,0 +1,28 @@
+﻿using APICatalogo.Context;
+using APICatalogo.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace APICatalogo.Controllers
+{
+    [Route("api/[Controller]")]
+    [ApiController]
+    public class CategoriasController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public CategoriasController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Categoria>> GetCategorias() 
+        {
+            return _context.Categorias.AsNoTracking().ToList();
+        }
+    }
+}
