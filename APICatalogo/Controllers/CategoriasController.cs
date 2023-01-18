30e4416b98570add59039644aba22f420a7a38ac
rogerio.chaves <rogerio.chaves@kentech.com.br>
2023-01-18 09:08:15 -0300

    Continuação das tratativas de erros com try catch

--- APICatalogo/Controllers/CategoriasController.cs
+++ APICatalogo/Controllers/CategoriasController.cs
@@ -64,40 +64,68 @@ namespace APICatalogo.Controllers
         [HttpPost]
         public ActionResult Post([FromBody] Categoria categoria) 
         {
-            _context.Categorias.Add(categoria);
-            _context.SaveChanges();
-            return new CreatedAtRouteResult("ObterCategoria", new { id = categoria.CategoriaId }, categoria);
+            try
+            {
+                _context.Categorias.Add(categoria);
+                _context.SaveChanges();
+                return new CreatedAtRouteResult("ObterCategoria", new { id = categoria.CategoriaId }, categoria);
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                 "Erro ao tentar criar uma nova categoria");
+            }
+            
         }
 
 
         [HttpPut("{id}")]
         public ActionResult Put(int id, [FromBody] Categoria categoria)
         {
-            if (id != categoria.CategoriaId)
+            try
             {
-                return BadRequest();
-            }
+                if (id != categoria.CategoriaId)
+                {
+                    return BadRequest($"Não foi possivel alterar a categoria com ID = {id}");
+                }
 
-            _context.Entry(categoria).State = EntityState.Modified;
-            _context.SaveChanges();
-            return Ok();
+                _context.Entry(categoria).State = EntityState.Modified;
+                _context.SaveChanges();
+                return Ok($"A categoria com ID = {id} foi atualizada com sucesso");
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                $"Erro ao tentar atualizar a categoria categoria com id ={id}");
+            }
+           
         }
 
         [HttpDelete("{id}")]
         public ActionResult<Categoria> Delete(int id)
         {
-            var categoria = _context.Categorias.FirstOrDefault(c => c.CategoriaId == id);
-            //var produto = _context.Produtos.Find(id);            //O metodo find vai direto na memória e localiza o produto (mas o id deve ser a chave primaria da tabela).
 
-            if (categoria == null)
+            try
             {
-                return NotFound();
+                var categoria = _context.Categorias.FirstOrDefault(c => c.CategoriaId == id);
+                //var produto = _context.Produtos.Find(id);            //O metodo find vai direto na memória e localiza o produto (mas o id deve ser a chave primaria da tabela).
+
+                if (categoria == null)
+                {
+                    return NotFound($"A categoria com ID = {id} não foi encontrada");
+                }
+
+                _context.Categorias.Remove(categoria);
+                _context.SaveChanges();
+
+                return categoria;
             }
-
-            _context.Categorias.Remove(categoria);
-            _context.SaveChanges();
-
-            return categoria;
+            catch(Exception) 
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                    $"Erro ao tentar excluir a categoria categoria com id ={id}");
+            }
+            
         }
     }
 }
