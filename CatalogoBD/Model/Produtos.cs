a9189b00589414dbb3eea31a66b088e1a22619fa
rogerio.chaves <rogerio.chaves@kentech.com.br>
2023-02-07 13:49:04 -0300

    Ajuste na estrutura de pastas

---
+++ CatalogoBD/Model/Produtos.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using static CatalogoBD.Categorias;
+
+namespace CatalogoBD
+{
+    public class Produtos
+    {
+        [Table("Produtos")]
+        public class Produto
+        {
+            [Key]
+            public int ProdutoId { get; set; }
+
+            [Required]
+            [MaxLength(80)]
+            public string Nome { get; set; }
+
+            [Required]
+            [MaxLength(300)]
+            public string Descricao { get; set; }
+
+            [Required]
+            public double Preco { get; set; }
+
+            [Required]
+            [MaxLength(300)]
+            public string ImgURL { get; set; }
+
+            public float Estoque { get; set; }
+            public DateTime DataCadastro { get; set; }
+
+            public Categoria Categoria { get; set; }
+            public int CategoriaId { get; set; }
+        }
+    }
+}
