5839538c4257677c92d9e5ef6e45736310e6dd71
rogerio.chaves <rogerio.chaves@kentech.com.br>
2023-02-13 08:11:27 -0300

    Att Api 3.1 par 5.0

--- APICatalogo/Controllers/ProdutosController.cs
+++ APICatalogo/Controllers/ProdutosController.cs
@@ -1,82 +1,82 @@
-﻿//using APICatalogo.Context;
-//using APICatalogo.Models;
-//using Microsoft.AspNetCore.Mvc;
-//using Microsoft.EntityFrameworkCore;
-//using System.Collections;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Reflection.Metadata.Ecma335;
+﻿using APICatalogo.Context;
+using APICatalogo.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 
-//namespace APICatalogo.Controllers
-//{
-//    [Route("api/[Controller]")]
-//    [ApiController]
-//    public class ProdutosController : Controller
-//    {
-//        private readonly AppDbContext _context;
-//        public ProdutosController(AppDbContext contexto)
-//        {
-//            _context = contexto;
-//        }
+namespace APICatalogo.Controllers
+{
+    [Route("api/[Controller]")]
+    [ApiController]
+    public class ProdutosController : Controller
+    {
+        private readonly AppDbContext _context;
+        public ProdutosController(AppDbContext contexto)
+        {
+            _context = contexto;
+        }
 
-//        [HttpGet]
-//        public ActionResult<IEnumerable<Produto>> GetProdutos()
-//        {
-//            return _context.Produtos.AsNoTracking().ToList();
-//        }
+        [HttpGet]
+        public ActionResult<IEnumerable<Produto>> GetProdutos()
+        {
+            return _context.Produtos.AsNoTracking().ToList();
+        }
 
-//        [HttpGet("{id}", Name ="ObterProduto")]
-//        public ActionResult<Produto> GetProdutoById(int id) 
-//        {
-//            var produto = _context.Produtos.AsNoTracking().FirstOrDefault(p => p.ProdutoId == id);
-//            if(produto == null)
-//            {
-//                return NotFound();
-//            }
-//            return Ok(produto);
-//        }
+        [HttpGet("{id}", Name = "ObterProduto")]
+        public ActionResult<Produto> GetProdutoById(int id)
+        {
+            var produto = _context.Produtos.AsNoTracking().FirstOrDefault(p => p.ProdutoId == id);
+            if (produto == null)
+            {
+                return NotFound();
+            }
+            return Ok(produto);
+        }
 
-//        [HttpPost]
-//        public ActionResult Post([FromBody] Produto produto) 
-//        {
-//            //if (!ModelState.IsValid)
-//            //{
-//            //    return BadRequest(ModelState);
-//            //}
+        [HttpPost]
+        public ActionResult Post([FromBody] Produto produto)
+        {
+            //if (!ModelState.IsValid)
+            //{
+            //    return BadRequest(ModelState);
+            //}
 
-//            _context.Produtos.Add(produto);
-//            _context.SaveChanges();
-//            return new CreatedAtRouteResult("ObterProduto", new {id = produto.ProdutoId}, produto);
-//        }
+            _context.Produtos.Add(produto);
+            _context.SaveChanges();
+            return new CreatedAtRouteResult("ObterProduto", new { id = produto.ProdutoId }, produto);
+        }
 
-//        [HttpPut("{id}")]
-//        public ActionResult Put(int id, [FromBody] Produto produto) 
-//        {
-//            if(id != produto.ProdutoId) 
-//            {
-//                return BadRequest();
-//            }
+        [HttpPut("{id}")]
+        public ActionResult Put(int id, [FromBody] Produto produto)
+        {
+            if (id != produto.ProdutoId)
+            {
+                return BadRequest();
+            }
 
-//            _context.Entry(produto).State = EntityState.Modified;
-//            _context.SaveChanges();
-//            return Ok();
-//        }
+            _context.Entry(produto).State = EntityState.Modified;
+            _context.SaveChanges();
+            return Ok();
+        }
 
-//        [HttpDelete("{id}")]
-//        public ActionResult<Produto> Delete(int id) 
-//        { 
-//            var produto = _context.Produtos.FirstOrDefault(p => p.ProdutoId == id);
-//            //var produto = _context.Produtos.Find(id);            //O metodo find vai direto na memória e localiza o produto (mas o id deve ser a chave primaria da tabela).
+        [HttpDelete("{id}")]
+        public ActionResult<Produto> Delete(int id)
+        {
+            var produto = _context.Produtos.FirstOrDefault(p => p.ProdutoId == id);
+            //var produto = _context.Produtos.Find(id);            //O metodo find vai direto na memória e localiza o produto (mas o id deve ser a chave primaria da tabela).
 
-//            if(produto == null)
-//            {
-//                return NotFound();
-//            }
+            if (produto == null)
+            {
+                return NotFound();
+            }
 
-//            _context.Produtos.Remove(produto);
-//            _context.SaveChanges();
+            _context.Produtos.Remove(produto);
+            _context.SaveChanges();
 
-//            return produto;
-//        }
-//    }
-//}
+            return produto;
+        }
+    }
+}
